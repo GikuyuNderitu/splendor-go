fb5c69bfed5da1b95f07c13c3470d54e1f2d8c16
GikuyuNderitu <gikuyunderitu@gmail.com>
2024-12-26 14:31:53 -0800

    Update service definition with comments and contentful impl of CreateTable rpc

--- server/go.mod
+++ server/go.mod
@@ -4,7 +4,7 @@ go 1.22.0
 
 require (
 	buf.build/gen/go/atypicaldev/splendorapis/connectrpc/go v1.17.0-20241226103919-8a750350418c.1
-	buf.build/gen/go/atypicaldev/splendorapis/protocolbuffers/go v1.36.1-20241226103919-8a750350418c.1
+	buf.build/gen/go/atypicaldev/splendorapis/protocolbuffers/go v1.36.1-20241226231523-90c010d37edd.1
 	connectrpc.com/connect v1.17.0
 	golang.org/x/net v0.23.0
 )
--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 
 	spv1 "buf.build/gen/go/atypicaldev/splendorapis/protocolbuffers/go/atypicaldev/splendorapis/v1"
 
@@ -14,7 +15,18 @@ func (s *SplendorService) CreateTable(
 	ctx context.Context,
 	req *connect.Request[spv1.CreateTableRequest],
 ) (*connect.Response[spv1.CreateTableResponse], error) {
-	res := connect.NewResponse(&spv1.CreateTableResponse{})
+	msg := req.Msg
+	fmt.Printf("CreateTable request: %v\n", msg)
+	tableId := "say_hello"
+	table := spv1.Table_builder{
+		TableId: &tableId,
+		Players: []*spv1.Player{
+			{Id: msg.CreatorId},
+		},
+	}.Build()
+	res := connect.NewResponse(&spv1.CreateTableResponse{
+		Table: table,
+	})
 
 	return res, nil
 }
