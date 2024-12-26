2988e63c2ba289497502d3e2fa8ae9285c86248e
GikuyuNderitu <gikuyunderitu@gmail.com>
2024-12-26 13:59:52 -0800

    Update run script to start connect server

--- server/pkg/setup/run.go
+++ server/pkg/setup/run.go
@@ -1,8 +1,13 @@
 package setup
 
 import (
+	"atypicaldev/splendor-go/internal/server"
 	"fmt"
 	"net/http"
+
+	spv1connect "buf.build/gen/go/atypicaldev/splendorapis/connectrpc/go/atypicaldev/splendorapis/v1/splendorapisv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 type ServerOpts struct {
@@ -10,14 +15,16 @@ type ServerOpts struct {
 }
 
 func Run(opts ServerOpts) {
+	svc := &server.SplendorService{}
+	route, handler := spv1connect.NewSplendorServiceHandler(svc)
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	}))
+	mux.Handle(route, handler)
 
 	fmt.Printf("Starting server with address: %s\n", opts.Addr)
-	if err := http.ListenAndServe(opts.Addr, mux); err != nil {
+	if err := http.ListenAndServe(opts.Addr, h2c.NewHandler(mux, &http2.Server{})); err != nil {
 		fmt.Printf("Error with server, shutting down: %v", err)
 	}
+
 }
